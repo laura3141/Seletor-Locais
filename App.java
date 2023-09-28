f5430ba40c685ed5a544374d3f73c691962f6962
Laura Costa <laura.costa3141@gmail.com>
2023-09-28 14:20:27 -0300

    organizacao

--- App.java
+++ App.java
@@ -5,75 +5,94 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+
 public class App {
     static Scanner scanner=new Scanner(System.in);
+
+    //Método para exibir o menu inicial
     public static int menu(){
         int op; 
         do {
-            System.out.println("\n0-Sair\n1-Cadastrar local\n2-Marcar local como visitado\n3-Escolher um local para ir\n4-Deletar um local\nDigite sua opcao: ");
+            System.out.print("---------MENU 1--------");
+            System.out.println("\n0 - Sair\n1 - Cadastrar local\n2 - Marcar local como visitado\n3 - Escolher um local para ir\n4 - Deletar um local");
+            System.out.println("-------------------------");
+            System.out.print("Digite sua opcao: ");
             op=scanner.nextInt();
             if(op > 4 || op < 0)System.out.println("Opcao invalida!");
         } while (op > 4 || op < 0);
         return op;
     }
     
+    //Método para ler um local a ser cadastrado
     public static String le(){
         String tipo;
         String nome;
         String preco;
         String inedito;
         String turno;
-        String total="";
+        String total="\n";
         
+        //Leitura do tipo
         int tipoI;
         do{
-            System.out.println("0 - restaurante");
-            System.out.println("1 - barzinho");
-            System.out.println("2 - balada");
-            System.out.println("3 - cafe");
-            System.out.println("4- ar livre");
+            System.out.println("---------MENU 2--------");
+            System.out.println("0 - Restaurante");
+            System.out.println("1 - Barzinho");
+            System.out.println("2 - Balada");
+            System.out.println("3 - Cafe");
+            System.out.println("4 - Ar livre");
+            System.out.println("-------------------------");
             System.out.print("Digite o tipo do local de acordo com os codigos: ");
             scanner.nextLine();
             tipo=scanner.nextLine();
             tipoI=Integer.parseInt(tipo);
-            if(tipoI>4||tipoI<0)System.out.println("Opcao invalida");
+            if(tipoI>4||tipoI<0)System.out.println("\nOpcao invalida!");
         }while(tipoI>4||tipoI<0);
         total=total+tipo+",";
 
-        int precoI;
-        System.out.println("Digite o nome do local:");
-        nome=scanner.nextLine();
-        total=total+nome+",";
-
+        //Leitura do nome
+        String nomeU;
         do{
-            System.out.print("Qual o preço[1-3] sendo 1 barato e 3 caro?");
+            System.out.print("Digite o nome do local: ");
+            nome=scanner.nextLine();
+            if(nome.length()>20)System.out.println("\nErro! Maximo 20 caracteres");
+        }while(nome.length()>20);
+        nomeU=nome.toUpperCase();
+        total=total+nomeU+",";
+
+        //Leitura do preço
+        int precoI;
+        do{
+            System.out.print("Qual o preço [1-3] sendo 1 barato e 3 caro? ");
             preco = scanner.nextLine();
             precoI=Integer.parseInt(preco);
-            if(precoI>3||precoI<0)System.out.println("Opcao invalida");
+            if(precoI>3||precoI<0)System.out.println("\nOpcao invalida! ");
         }while(precoI>3||precoI<0);
         total=total+preco+",";
+        
+        //Leitura se o local é inedito ou nao
         String ineditoU;
-
         do{
-            System.out.print("O local é inédito?[SIM/NAO]");
+            System.out.print("O local é inédito [SIM/NAO]? ");
             inedito=scanner.nextLine();
             ineditoU=inedito.toUpperCase();
-            if(!ineditoU.equals("SIM")&&!ineditoU.equals("NAO"))System.out.println("Opcao invalida");
+            if(!ineditoU.equals("SIM")&&!ineditoU.equals("NAO"))System.out.println("Opcao invalida! ");
         }while(!ineditoU.equals("SIM")&&!ineditoU.equals("NAO"));
         total=total+ineditoU+",";
 
+        //Leitura do turno
         String turnoU;
         do{
-            System.out.println("O local é para dia, noite ou ambos?");
+            System.out.print("O local é para dia, noite ou ambos? ");
             turno=scanner.nextLine();
             turnoU=turno.toUpperCase();
-            if(!turnoU.equals("AMBOS")&&!turnoU.equals("DIA")&&!turnoU.equals("NOITE"))System.out.println("Opcao invalida");
+            if(!turnoU.equals("AMBOS")&&!turnoU.equals("DIA")&&!turnoU.equals("NOITE"))System.out.println("Opcao invalida!");
         }while(!turnoU.equals("AMBOS")&&!turnoU.equals("DIA")&&!turnoU.equals("NOITE"));
-        total=total+turnoU +",";
+        total=total+turnoU;
         return total;
     }
     
-
+    //Método para alterar o status inédito
     public static void marcaVisita(String nome){
         try (BufferedReader leitor = new BufferedReader(new FileReader("locais.txt"))) {
             String linha;//guarda o conteudo da linha atual
@@ -98,13 +117,13 @@ public class App {
                     linhaUt=linha;
                     novaLinha=linha.replace("SIM","NAO");
                 }
+                sc2.close();
             }
             String novoArquivo=arquivo.replace(linhaUt, novaLinha);
     
             try (FileWriter fileWriter = new FileWriter("locais.txt")) {
                 fileWriter.write(novoArquivo);
-                fileWriter.write("\n");
-                System.out.println("Dados foram atualizados com sucesso no arquivo.");
+                System.out.println("\nAtualizacao realizada com sucesso!\n");
             } catch (IOException e) {   
                 e.printStackTrace();
             }
@@ -112,6 +131,8 @@ public class App {
             e.printStackTrace();
         }
     }
+
+    //Método para excluir um local
     public static void exclui(String nome){
         try (BufferedReader leitor = new BufferedReader(new FileReader("locais.txt"))) {
             String linha;//guarda o conteudo da linha atual
@@ -137,7 +158,7 @@ public class App {
            
             try (FileWriter fileWriter = new FileWriter("locais.txt")) {
                 fileWriter.write(arquivo);
-                System.out.println("Local excluido com sucesso.");
+                System.out.println("\nLocal excluido com sucesso!\n ");
             } catch (IOException e) {   
                 e.printStackTrace();
             }
@@ -146,13 +167,13 @@ public class App {
         }
     }
 
+    //Método para cadastrar um local no arquivo
     public static void escreveArquivo(String str){
         String nomeArq = "locais.txt";
         
         try (FileWriter fileWriter = new FileWriter(nomeArq,true)) {
             fileWriter.write(str);
-            fileWriter.write("\n");
-            System.out.println("Dados foram escritos com sucesso no arquivo.");
+            System.out.println("\nLocal cadastrado com sucesso!\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -168,25 +189,29 @@ public class App {
             if(opcao!=0){
                 switch (opcao){
                     case 0:
-                        System.out.println("Até a próxima! \2 ");
+                        System.out.println("\nAté a próxima!\n");
                             break;
                     case 1:
                         escreveArquivo(le());
                             break;
 
                     case 2:
-                        System.out.print("Digite o nome do local que voce quer marcar como visitado: ");
+                        System.out.print("Digite o nome do local que voce deseja marcar como visitado: ");
                         scanner.nextLine();
                         nome=scanner.nextLine();
-                        marcaVisita(nome);
+                        String nomeU=nome.toUpperCase();
+                        marcaVisita(nomeU);
                             break;
                     case 3:
-                        System.out.print("Digite o nome do local que voce quer marcar como visitado: ");
+                        
+                             break;
+                    case 4:
+                        System.out.print("Digite o nome do local que voce deseja excluir: ");
                         scanner.nextLine();
                         nome=scanner.nextLine();
-                        exclui(nome);
-
-                             break;
+                        String nomeM=nome.toUpperCase();
+                        exclui(nomeM);
+                            break;
                         
                 }
             }
